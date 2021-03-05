efc576c491c9bdd481bd80d63da41db22c4d191a
Lucía <lucialamalba@gmail.com>
2021-03-05 15:03:52 +0100

    Comentarios en la práctica

--- practicas/formulario_contacto/index.php
+++ practicas/formulario_contacto/index.php
@@ -3,10 +3,15 @@
 $errors = '';
 $sent = '';
 
+//En caso de que el formulario tenga algún contenido aportado por el usuario, inicializamos sus variables:
+
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
+
+//En caso de que la variable "name" no esté vacía, "limpiamos" el dato que hayan introducido, tanto de espacios como de código html inyectado etc.:
+//Si está vacía, añadiremos el mensaje de error correspondiente, a la variable "errors", para usarla después:
 
   if (!empty($name)) {
     $name = trim($name);
@@ -14,6 +19,10 @@ if (isset($_POST['submit'])) {
   } else {
     $errors .= 'Please, write your name </br>';
   }
+
+//En caso de que la variable "email" no esté vacía, usamos un filtro para eliminar caracteres no válidos, y otro de validación del email.
+//Si la validación no es correcta, sumamos el mensaje correspondiente a la variable "errors"
+//Si la variable está vacía, sumamos un mensaje de error a la variable "errors":
 
   if (!empty($email)) {
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -25,6 +34,8 @@ if (isset($_POST['submit'])) {
     $errors .= 'Sorry but we need to know your email </br>';
   }
 
+//Si la variable mensaje no está vacía, corregimos los espacios y caracteres. Si está vacía, añadimos el mensaje de error a la variable "errors".
+
   if (!empty($message)) {
     $message = htmlspecialchars($message);
     $message = trim($message);
@@ -32,6 +43,8 @@ if (isset($_POST['submit'])) {
   } else {
     $errors .= 'Oops, you forgot to write your message </br>';
   }
+
+//En caso de que no haya errores, construimos el mensaje para enviarlo a cierto mail de recepción del formulario:
 
   if (!$errors) {
     $send_to = 'anemail@adirection.com';
@@ -41,10 +54,15 @@ if (isset($_POST['submit'])) {
     $built_message .= "Message: " . $message;
 
     //mail($send_to, $issue, $built_message);
+
+  //Y marcamos la variable $sent como true, para utilizarla después en el archivo hmtl:
+
     $sent = 'true';
   }
 
 }
+
+//Ponemos AL FINAL DEL ARCHIVO, el require con el archivo del front de la web. Para unir la lógica con el "cuerpo" de la página web:
 
 require 'index.view.php';
 ?>
--- practicas/formulario_contacto/index.view.php
+++ practicas/formulario_contacto/index.view.php
@@ -9,6 +9,7 @@
   </head>
   <body>
     <div class="wrap">
+      <!-- Para que la web se "recargue sobre sí misma". No nos envíe a ningún otro sitio cuando enviemos el formulario -->
       <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         <input type="text"
           class="form-control"
@@ -16,13 +17,18 @@
           name="name"
           placeholder="Write your name here:"
           value="<?php if(!$sent && isset($name))echo $name?>">
+          <!-- Si el formulario no ha sido enviado, conservaremos el nombre que hayan escrito, para que no tengan que volver a hacerlo -->
         <input type="text"
           class="form-control"
           id="email" name="email"
           placeholder="And here, your email:"
           value="<?php if(!$sent && isset($email))echo $email?>">
+          <!-- Si el formulario no ha sido enviado, conservaremos el email que hayan escrito, para que no tengan que volver a hacerlo -->
         <textarea name="message" class="form-control" id="menssage" placeholder="Now you can write your message:"><?php if(!$sent && isset($message))echo $message?></textarea>
-        <!--
+          <!-- Si el formulario no ha sido enviado, conservaremos el nombre que hayan escrito, para que no tengan que volver a hacerlo -->
+
+
+        <!-- Esto fue una prueba para maquetar los errores.
         <div class="alert error">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
@@ -30,15 +36,23 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
         -->
+
+        <!-- PARA MOSTRAR LOS ERRORES -->
+        <!-- En caso de que la variable errores no esté vacía, creo un div en el que envío el código php con los mensajes de error -->
+
         <?php if (!empty($errors)): ?>
           <div class="alert error">
             <?php echo $errors; ?>
           </div>
+
+        <!-- En caso de que la variable $sent sea true, enviamos un mensaje de que el formulario ha sido enviado -->
         <?php elseif($sent): ?>
           <div class="alert success">
             <p>Your request was sent correctly</p>
           </div>
         <?php endif ?>
+        
+        <!-- Botón del submit -->
         <input type="submit" name="submit" class="btn" value="Send it!">
       </form>
     </div>
